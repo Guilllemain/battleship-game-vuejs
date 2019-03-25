d4820ddcd79b671e31dbeacde445656f0d4140f0
Yann Guillemain <devguillemain@gmail.com>
2019-03-25 17:30:39 +0100

    Adding styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <div style="display:flex;">
+        <div class="container">
+            <div class="side side--human">
+
             <div class="ships">
                 <div class="ships__list">
                     <div
@@ -8,31 +9,32 @@
                         @click="selectShip(ship)"
                         :key="ship.id"
                         class="ships__size"
-                        :class="[`ships__size--${ship.length}`, {'not-available': ship.human.isReady }]"
+                        :class="[`ships__size--${ship.length}`, {'not-available': ship.human.isReady || ship.computer.isSinked }]"
                     >{{ ship.name }}</div>
-                    <button @click="changeShipDirection">Change ship direction</button>
-                    <button :disabled="!isPlaying" @click="startGame">Start Game</button>
-                    <button @click="placeShipsRandom">Place ships randomly</button>
                 </div>
+                <template v-if="!isPlaying">
+                    <button class="button" :disabled="!selectedShip" :class="{ 'button--disabled': !selectedShip }" @click="changeShipDirection">Change ship direction</button>
+                    <button class="button" :disabled="!isPlayerReady" :class="{ 'button--disabled': !isPlayerReady }" @click="startGame">Start Game</button>
+                    <button class="button" @click="placeShipsRandom">Place ships randomly</button>
+                </template>
             </div>
-            <div class="grid" style="margin-right:10rem">
-                <!-- eslint-disable-next-line -->
+            <div class="grid grid--human" style="margin-right:10rem">
                 <div
-                    v-for="cell in grid"
-                    @mouseover="moveShipOnGrid"
+                    v-for="cell in grid" :key="cell.id"
+                    @mouseover="moveShipOnGrid({x: cell.x, y: cell.y})"
                     @mouseout="removeShip"
                     @click="addShipIdToGrid({x: cell.x, y: cell.y}, grid)"
-                    :data-column="`${cell.y}`"
-                    :data-row="`${cell.x}`"
                     class="grid__cell"
-                    :class="{ 'grid__cell--hit': cell.hit, 'grid__cell--placed': !cell.isEmpty && !cell.hit, 'grid__cell--ship': cell.isHovered, 'grid__cell--missed': cell.missed}"
+                    :class="{ 'grid__cell--hit': cell.hit, 'grid__cell--placed': !cell.isEmpty && !cell.hit, 'grid__cell--hovering': cell.isHovered, 'grid__cell--missed': cell.missed}"
                 ></div>
             </div>
-            <div v-if="isPlaying && AIGrid.length > 0" style="display:flex">
-                <div class="grid">
-                    <!-- eslint-disable-next-line -->
+            </div>
+            <transition name="slide">
+                <div v-show="isPlaying && AIGrid.length > 0" class="side side--computer">
+
+                <div class="grid grid--computer">
                     <div
-                        v-for="cell in AIGrid"
+                        v-for="cell in AIGrid" :key="cell.id"
                         @click="attackShip({x: cell.x, y: cell.y})"
                         class="grid__cell"
                         :class="{ 'grid__cell--hit': cell.hit, 'grid__cell--missed': cell.missed }"
@@ -49,9 +51,11 @@
                         >{{ ship.name }}</div>
                     </div>
                 </div>
-            </div>
+                </div>
+            </transition>
+
+
         </div>
-    </div>
 </template>
 
 <script>
@@ -59,6 +63,7 @@ import shipsList from "./assets/js/ships";
 export default {
     data() {
         return {
+            isPlaying: false,
             gridSize: 10,
             ships: shipsList,
             firstHit : '',
@@ -67,7 +72,7 @@ export default {
         };
     },
     computed: {
-        isPlaying() {
+        isPlayerReady() {
             return this.ships.every(ship => ship.human.isReady);
         },
         selectedShip() {
@@ -80,9 +85,9 @@ export default {
                 cell => cell.x === coordinates.x && cell.y === coordinates.y
             );
             const ship = this.ships[cell.shipId];
-            if (cell.isEmpty) {
+            if (cell.isEmpty && !cell.missed) {
                 cell.missed = true;
-            } else if (cell.hit) {
+            } else if (cell.hit || cell.missed) {
                 return    
             } else {
                 cell.hit = true;
@@ -91,8 +96,11 @@ export default {
                 if (ship.length === ship.human.hitCounter)
                     ship.human.isSinked = true;
                 // check all ships are sinked
-                if (this.ships.every(ship => ship.human.isSinked))
-                    alert("You won");
+                if (this.ships.every(ship => ship.human.isSinked)) {
+                    alert("You won")
+                    this.resetGame()
+                    this.createGrid(this.grid)
+                }
             }
             this.AIAttack();
         },
@@ -103,6 +111,12 @@ export default {
             let cell = this.grid.find(
                 cell => cell.x === randomX && cell.y === randomY
             )
+
+            if (!this.firstHit) {
+                if (this.grid.filter(target => target.y === randomY && (target.x === randomX + 1 || target.x === randomX - 1) ||
+                    target.x === randomX && (target.y === randomY + 1 || target.y === randomY - 1))
+                    .some(target => target.missed)) return this.AIAttack()
+            }
             
             if (this.firstHit && this.grid.some(cell => cell.prob === 1)) {
                 const cells = this.grid.filter(cell => cell.prob === 1)
@@ -146,8 +160,11 @@ export default {
                 this.grid.forEach(cell => cell.prob = 0)
             }
             
-            if (this.ships.every(ship => ship.computer.isSinked))
-                alert("You lose");
+            if (this.ships.every(ship => ship.computer.isSinked)) {
+                alert("You lose")
+                this.resetGame()
+                this.createGrid(this.grid)
+            }
         },
         addShipIdToGrid(coordinates, grid) {
             if (!this.selectedShip) return;
@@ -213,20 +230,18 @@ export default {
         removeShip() {
             this.grid.forEach(cell => (cell.isHovered = false));
         },
-        moveShipOnGrid() {
+        moveShipOnGrid(coordinates) {
             if (!this.selectedShip) return;
-            const mouseX = Number(event.target.dataset.row);
-            const mouseY = Number(event.target.dataset.column);
 
             if (!this.selectedShip.isVertical) {
-                if (mouseX + this.selectedShip.length > 10) return;
+                if (coordinates.x + this.selectedShip.length > 10) return;
                 // check if there is already a ship in the hover area
                 if (
                     this.grid.some(
                         cell =>
-                            cell.y === mouseY &&
-                            cell.x >= mouseX &&
-                            cell.x < mouseX + this.selectedShip.length &&
+                            cell.y === coordinates.y &&
+                            cell.x >= coordinates.x &&
+                            cell.x < coordinates.x + this.selectedShip.length &&
                             !cell.isEmpty
                     )
                 )
@@ -235,20 +250,20 @@ export default {
                 this.grid
                     .filter(
                         cell =>
-                            cell.y === mouseY &&
-                            cell.x >= mouseX &&
-                            cell.x < mouseX + this.selectedShip.length
+                            cell.y === coordinates.y &&
+                            cell.x >= coordinates.x &&
+                            cell.x < coordinates.x + this.selectedShip.length
                     )
                     .forEach(cell => (cell.isHovered = true));
             } else {
-                if (mouseY + this.selectedShip.length > 10) return;
+                if (coordinates.y + this.selectedShip.length > 10) return;
                 // check if there is already a ship in the hover area
                 if (
                     this.grid.some(
                         cell =>
-                            cell.x === mouseX &&
-                            cell.y >= mouseY &&
-                            cell.y < mouseY + this.selectedShip.length &&
+                            cell.x === coordinates.x &&
+                            cell.y >= coordinates.y &&
+                            cell.y < coordinates.y + this.selectedShip.length &&
                             !cell.isEmpty
                     )
                 )
@@ -257,9 +272,9 @@ export default {
                 this.grid
                     .filter(
                         cell =>
-                            cell.x === mouseX &&
-                            cell.y >= mouseY &&
-                            cell.y < mouseY + this.selectedShip.length
+                            cell.x === coordinates.x &&
+                            cell.y >= coordinates.y &&
+                            cell.y < coordinates.y + this.selectedShip.length
                     )
                     .forEach(cell => (cell.isHovered = true));
             }
@@ -296,17 +311,21 @@ export default {
                 this.selectedShip.isVertical = !this.selectedShip.isVertical;
         },
         startGame() {
+            this.isPlaying = true
             this.createAIGrid();
             this.placeAIShips();
         },
         placeAIShips() {
             this.ships.forEach(ship => (ship.human.isReady = false));
             this.generateRandomCoordinates(this.AIGrid);
+            this.ships.forEach(ship => (ship.human.isReady = false));
         },
         createGrid(grid) {
+            let id = 1
             for (let y = 0; y < this.gridSize; y++) {
                 for (let x = 0; x < this.gridSize; x++) {
                     grid.push({
+                        id: id++,
                         x: x,
                         y: y,
                         hit: false,
@@ -323,7 +342,14 @@ export default {
         resetGame() {
             this.grid = [],
             this.AIGrid = [],
-            this.firstHit = ''
+            this.firstHit = '',
+            this.ships.forEach(ship => {
+                ship.human.isSinked = false
+                ship.human.hitCounter = 0
+                ship.human.isReady = false
+                ship.computer.isSinked = false
+                ship.computer.hitCounter = 0
+            })
         }
     },
     created() {
@@ -333,12 +359,79 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700');
+
+* {
+    padding: 0;
+    margin: 0;
+}
+
+body, html {
+    height: 100%;
+}
+
+body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #eeeeee;
+}
+
+$cellWidth: 3rem;
+$blue: #348fda;
+$grey: #a7a7a7;
+$red: #b90000;
+
+.container {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+}
+
+.side {
+    display: flex;
+
+    &--human {
+        justify-self: start;
+    }
+}
+
 .not-available {
     opacity: 0.5;
-    background-color: grey;
+    background-color: $grey;
+}
+.button {
+    cursor: pointer;
+    border: none;
+    padding: 1rem 1.5rem;
+    background-color: $blue;
+    border-radius: 3px;
+    color: whitesmoke;
+    font-weight: 700;
+    transition: all .3s ease-in-out;
+    outline: none;
+
+    &:hover:not(&--disabled) {
+        transform: translateY(-2px);
+        background-color: darken($blue, 10%);
+    }
+
+    &--disabled {
+        background-color: lighten($blue, 20%);
+    }
+
+    &:active {
+        transform: translateY(1px);
+    }
+
+    &:not(:last-of-type) {
+        margin-bottom: .8rem;
+    }
 }
 .ships {
-    width: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
     &__list {
         height: 17rem;
     }
@@ -347,22 +440,29 @@ export default {
         align-items: center;
         justify-content: center;
         margin: 0.2rem 0;
-        background-color: coral;
-        height: 3rem;
+        cursor: pointer;
+        background-color: $grey;
+        height: $cellWidth;
+        border-radius: $cellWidth;
+
+        &:hover:not(.not-available) {
+            background-color: darken($grey, 10%);
+        }
+
         &--1 {
-            width: 3rem;
+            width: $cellWidth;
         }
         &--2 {
-            width: 6rem;
+            width: $cellWidth * 2;
         }
         &--3 {
-            width: 9rem;
+            width: $cellWidth * 3;
         }
         &--4 {
-            width: 12rem;
+            width: $cellWidth * 4;
         }
         &--5 {
-            width: 15rem;
+            width: $cellWidth * 5;
         }
     }
 }
@@ -373,32 +473,62 @@ export default {
     grid-template-columns: repeat(10, 1fr);
     grid-template-rows: repeat(10, 1fr);
     grid-gap: 3px;
-    background-color: blue;
-    border: 3px blue solid;
+    border-radius: 1%;
+    
+
+    &--human {
+        border: 3px $blue solid; 
+        background-color: $blue;
+
+        & .grid__cell {
+            cursor: default;
+        }
+    }
+
+    &--computer {
+        background-color: lightgray;
+        border: 3px lightgray solid;
+
+        & .grid__cell:hover:not(.grid__cell--hit):not(.grid__cell--missed) {
+            background-color: $grey;
+        }
+    }
 
     &__cell {
+        display: flex;
+        align-items: center;
+        justify-content: center;
         cursor: pointer;
         background-color: whitesmoke;
+        width: 95%;
+        height: 95%;
+        border-radius: 50%;
+        transition: all .2s ease-in-out;
 
         &--hit {
-            background-color: black;
+            background-color: $red;
+            cursor: not-allowed;
         }
 
-        &--ship {
-            background-color: greenyellow;
+        &--hovering {
+            background-color: $grey;
         }
 
         &--missed {
-            background-color: orange;
+            background-color: transparent;
+            cursor: not-allowed;
         }
 
         &--placed {
-            background-color: green;
-        }
-
-        &:hover {
-            // background-color: grey;
+            background-color: darken($grey, 20%);
         }
     }
+}
+.slide-enter-active {
+    transition: all .4s ease-in-out;
+}
+.slide-enter {
+    opacity: 0;
+    transform: translateX(20vw);
 }
 </style>
